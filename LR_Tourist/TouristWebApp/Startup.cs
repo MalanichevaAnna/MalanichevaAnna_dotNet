0af4558998688afdf282a60a7a48373345c62d94
MalanichevaAnna <48821498+MalanichevaAnna@users.noreply.github.com>
2020-05-03 23:09:39 +0300

    Update Startup.cs

--- LR_Tourist/TouristWebApp/Startup.cs
+++ LR_Tourist/TouristWebApp/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using AutoMapper;
 using BLL;
 using DA;
 using DA.Services.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
