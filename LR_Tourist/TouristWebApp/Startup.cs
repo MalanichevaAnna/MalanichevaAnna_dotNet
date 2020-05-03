62e5ef0a46381db55dbf114a53c202b96d71d71d
MalanichevaAnna <48821498+MalanichevaAnna@users.noreply.github.com>
2020-05-03 23:08:36 +0300

    Update Startup.cs

--- LR_Tourist/TouristWebApp/Startup.cs
+++ LR_Tourist/TouristWebApp/Startup.cs
@@ -60,12 +60,12 @@ namespace TouristWebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            //app.UseHttpsRedirection();
+            //app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
