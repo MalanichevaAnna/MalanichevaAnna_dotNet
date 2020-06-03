1a8a4d41ad87735b4babed5cea3b3107f458da94
MalanichevaAnna <48821498+MalanichevaAnna@users.noreply.github.com>
2020-06-03 11:21:04 +0300

    fixed

--- LR_Tourist/TouristWebAPI/Program.cs
+++ LR_Tourist/TouristWebAPI/Program.cs
@@ -9,15 +9,9 @@ namespace TouristWebAPI
 {
     public class Program
     {
-        public static async Task Main(string[] args)
-            {
+        public static void Main(string[] args)
+        {
             var host = CreateHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-            }
-
             host.Run();
         }
 
