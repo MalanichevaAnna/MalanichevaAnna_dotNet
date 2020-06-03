ab5dbd5cd215c13affe62c4f585b11c31e554794
MalanichevaAnna <48821498+MalanichevaAnna@users.noreply.github.com>
2020-06-03 11:22:05 +0300

    fixed

--- LR_Tourist/TouristWebAPI/Program.cs
+++ LR_Tourist/TouristWebAPI/Program.cs
@@ -1,9 +1,5 @@
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using TouristWebApp;
 
 namespace TouristWebAPI
 {
